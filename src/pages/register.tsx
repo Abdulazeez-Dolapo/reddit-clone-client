efba8ef190c987f1735254a377a1a1abc72724c3
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-12-05 05:37:03 +0100

    feature: add the register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,36 +1,61 @@
 import React from "react"
-
 import { Formik, Form } from "formik"
-import {
-	FormControl,
-	FormLabel,
-	Input,
-	// FormErrorMessage,
-} from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 
 import Wrapper from "../components/Wrapper"
+import { InputField } from "../components/InputField"
+
+import { useRegisterMutation } from "../generated/graphql"
+
+import { toErrorMap } from "../utils/toErrorMap"
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+	const [, register] = useRegisterMutation()
+
+	const router = useRouter()
+
 	return (
 		<Wrapper variant="small">
 			<Formik
 				initialValues={{ username: "", password: "" }}
-				onSubmit={values => console.log(values)}
+				onSubmit={async (values, { setErrors }) => {
+					const response = await register(values)
+
+					if (response.data?.register.errors) {
+						setErrors(toErrorMap(response.data.register.errors))
+					} else if (response.data?.register.user) {
+						router.push("/")
+					}
+				}}
 			>
-				{({ values, handleChange }) => (
+				{({ isSubmitting }) => (
 					<Form>
-						<FormControl>
-							<FormLabel htmlFor="username">Username</FormLabel>
-							<Input
-								value={values.username}
-								onChange={handleChange}
-								id="username"
-								placeholder="username"
+						<InputField
+							name="username"
+							placeholder="username"
+							label="Username"
+						/>
+
+						<Box mt={4}>
+							<InputField
+								name="password"
+								placeholder="password"
+								type="password"
+								label="Password"
 							/>
-							{/*  <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-						</FormControl>
+						</Box>
+
+						<Button
+							mt={4}
+							colorScheme="teal"
+							type="submit"
+							isLoading={isSubmitting}
+						>
+							Register
+						</Button>
 					</Form>
 				)}
 			</Formik>
